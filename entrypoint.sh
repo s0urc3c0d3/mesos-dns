7575b292a742463fa807c98b597731ea6f330454
Grzegorz Dwornicki <gd1100@gmail.com>
2017-07-21 15:29:54 +0200

    Added first version of entrypoint.sh and Docker file

---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo >> /config.json <<EOF 
+{
+	"zk":"zk://$ZK_ADDRESSES/mesos",
+		"masters":[$MESOS_MASTERS]",
+		"stateTimeoutSeconds":$stateTimeoutSeconds,
+		"refreshSeconds":$refreshSeconds,
+		"ttl":$ttl,
+		"domain":"$MESOS_DOMAIN",
+		"ns":"ns1",
+		"port":$PORT,
+		"resolvers":[$RESOLVERS],
+		"timeout":$timeout,
+		"listener":"$listener",
+		"SOAMname":"root.ns1.$MESOS_DOMAIN",
+		"SOARefresh":$SOARefresh,
+		"SOARetry":$SOARetry,
+		"SOAExpire":$SOAExpire,
+		"SOAMinttl":$SOAMinttl,
+		"dnson":true,
+		"httpon":true,
+		"httpport":$HTTP_PORT,
+		"externalon":true,
+		"recurseon":true,
+		"IPSources":["mesos","host"],
+		"EnforceRFC952":false,
+		"EnumerationOn":true
+}
+EOF
+
+exec /usr/bin/mesos-dns -v=2 -config=/config.json
